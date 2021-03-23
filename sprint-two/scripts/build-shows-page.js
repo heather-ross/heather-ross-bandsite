58e441c5d922025f61347ae6b1ee9ae3c40efe44
heather-ross <hross299@yahoo.com>
2021-03-22 22:01:34 -0400

    tidied up JS a little and changed shows first col margin to 0 in desktop, instead of only desktop-xl

--- sprint-two/scripts/build-shows-page.js
+++ sprint-two/scripts/build-shows-page.js
@@ -36,10 +36,11 @@ let showsContainer = document.querySelector('.shows');
 let showsTitle = document.createElement('h2');
     showsTitle.classList.add('shows__title');
     showsTitle.innerText = 'Shows';
-    showsContainer.appendChild(showsTitle);
-
+    
 let showsWrap = document.createElement('section');
     showsWrap.classList.add('shows__wrap');
+    
+    showsContainer.appendChild(showsTitle);
     showsContainer.appendChild(showsWrap);
 
 
@@ -47,62 +48,64 @@ function displayShows(content) {
 
 let showsRow = document.createElement('div');
     showsRow.classList.add('shows__row');
-    showsWrap.appendChild(showsRow);
 
     //Date Wrap
 let showsDateWrap = document.createElement('div');
     showsDateWrap.classList.add('shows__content');
-    showsRow.appendChild(showsDateWrap);
 
 let showsHeadingDate = document.createElement('h5');
     showsHeadingDate.classList.add('shows__heading');
     showsHeadingDate.innerText = 'DATE';
-    showsDateWrap.appendChild(showsHeadingDate);
 
 let showsTextDate = document.createElement('p');
     showsTextDate.classList.add('shows__text');
     showsTextDate.innerText = content.showDate;
-    showsDateWrap.appendChild(showsTextDate);
 
     //Venue Wrap
 let showsVenueWrap = document.createElement('div');
     showsVenueWrap.classList.add('shows__content');
-    showsRow.appendChild(showsVenueWrap);
 
 let showsHeadingVenue = document.createElement('h5');
     showsHeadingVenue.classList.add('shows__heading');
     showsHeadingVenue.innerText = 'VENUE';
-    showsVenueWrap.appendChild(showsHeadingVenue);
 
 let showsTextVenue = document.createElement('p');
     showsTextVenue.classList.add('shows__text');
     showsTextVenue.innerText = content.showVenue;
-    showsVenueWrap.appendChild(showsTextVenue);
 
     //Location Wrap
 let showsLocationWrap = document.createElement('div');
     showsLocationWrap.classList.add('shows__content');
-    showsRow.appendChild(showsLocationWrap);
 
 let showsHeadingLocation = document.createElement('h5');
     showsHeadingLocation.classList.add('shows__heading');
     showsHeadingLocation.innerText = 'LOCATION';
-    showsLocationWrap.appendChild(showsHeadingLocation);
 
 let showsTextLocation = document.createElement('p');
     showsTextLocation.classList.add('shows__text');
     showsTextLocation.innerText = content.showLocation;
-    showsLocationWrap.appendChild(showsTextLocation);
 
 let showsBtn = document.createElement('button');
     showsBtn.classList.add('shows__button');
     showsBtn.innerText = "BUY TICKETS";
+
+    showsWrap.appendChild(showsRow);
+    showsRow.appendChild(showsDateWrap);
+    showsDateWrap.appendChild(showsHeadingDate);
+    showsDateWrap.appendChild(showsTextDate);
+    showsRow.appendChild(showsVenueWrap);
+    showsVenueWrap.appendChild(showsHeadingVenue);
+    showsVenueWrap.appendChild(showsTextVenue);
+    showsRow.appendChild(showsLocationWrap);
+    showsLocationWrap.appendChild(showsHeadingLocation);
+    showsLocationWrap.appendChild(showsTextLocation);
     showsRow.appendChild(showsBtn);
+
 }
 
  function addNewShow() { 
-    showsArray.forEach(function(showsArray) { 
-        displayShows(showsArray);
+    showsArray.forEach(function(show) { 
+        displayShows(show);
     }) 
 }
 addNewShow();
